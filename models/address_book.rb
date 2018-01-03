e03b99c0f9f4969fd9ba9c3c405450cb234415b7
Chris Boatwright <christopher.boatwright@gmail.com>
2018-01-02 22:07:12 -0500

    Commit 07.01

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 #http://ruby-doc.org/stdlib-2.0.0/libdoc/csv/rdoc/CSV.html#method-c-new
 #https://ruby-doc.org/core-2.2.2/Hash.html
+#https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 require_relative 'entry' #Tell Ruby to load the library named entry.rb relative to address_book.rb's file path using require_relative
 require "csv"
@@ -39,6 +40,33 @@ class AddressBook
           row_hash = row.to_hash #takes each row and turns it to hash, a dictionary-like object that stores keys and their values....
           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"]) #then fills those rows with add_entry method above, taking each name/phone_number/email value and putting them into their respective rows
         end   
-    end    
+    end
+    
+    # Search AddressBook for a specific entry by name, using binary (divide and conquer) algorithmic pattern
+    def binary_search(name)
+        
+        #first we establish the length of the array, thinking of it as a left to right list
+
+        lower = 0 #leftmost item in array
+        upper = entries.length - 1 #rightmost item in array
+        
+        
+        while lower <= upper #loop that says "While the lower index is less than or equal to the upper..."
+            mid = (lower + upper) / 2 #"...find the middle index by adding the lower and upper and dividing by two..." (NOTE: Ruby will truncate any decimal numbers)
+            mid_name = entries[mid].name #"...then take the middle index use it to retrive the name at that position"
+      
+            if name == mid_name
+              return entries[mid] #if you happen to land on the right index, stop right there
+            elsif name < mid_name 
+              upper = mid - 1 #if the middle index is alphabetically AFTER the name you're looking for, you need to go DOWN to the list and try again
+            elsif name > mid_name
+              lower = mid + 1 #if the middle index is alphabetically BEFORE the name you're looking for, you need to go UP to the list and try again
+            end
+        
+        end
+
+        return nil #if all the dividing and conquering finds nothing, return nil   
+
+    end
         
 end
